49d844fc0cc4e7a2379758db87728f734acd5713
laura.fierce@pnnl.gov <laura.fierce@pnnl.gov>
2026-04-20 16:49:42 -0700

    cleaning up some stale files

--- flexplot-grid-1/src/flexplot_grid/_version.py
+++
@@ -1,8 +0,0 @@
-"""
-Version information for the flexplot_grid package.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
-__description__ = "A flexible plotting package with grid layout helpers."
--- pyparticle-viz/src/PyParticle/viz/styling.py
+++
@@ -1,7 +0,0 @@
-def get_colors():
-    # Function to retrieve a list of colors for plotting
-    return ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-
-def get_linestyles():
-    # Function to retrieve a list of line styles for plotting
-    return ['-', '--', '-.', ':']
