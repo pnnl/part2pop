e033a38983d4184641cba98fbd7e6d641efd28b5
beelerpayton <beelerpayton@hotmail.com>
2026-04-22 07:22:53 -0600

    cleanup

--- tests/unit_old/analysis/particle/__init__.py
+++
@@ -1,1 +0,0 @@
-# particle analysis tests
--- tests/unit_old/freezing/test_base.py
+++
@@ -1,9 +0,0 @@
-# tests/unit/freezing/test_base.py
-
-from part2pop.freezing.base import retrieve_Jhet_val
-
-
-def test_retrieve_Jhet_val_for_known_species():
-    m, b = retrieve_Jhet_val("SO4", spec_modifications={})
-    float(m)
-    float(b)
